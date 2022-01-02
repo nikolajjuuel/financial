74856e06f376cdefbb36e05ab237f0e6db945931
Nikolaj Johansen <nikolaj.juuel@gmail.com>
2022-01-02 14:31:55 -0800

    install morgan and bodyparser middlewear

--- back-end/server.js
+++ back-end/server.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
+
+// Middleware
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 
 
 app.listen(3001, () => {
