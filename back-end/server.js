b13b42189a0714ef485a457556edce17e432b2d8
Nikolaj Johansen <nikolaj.juuel@gmail.com>
2022-01-02 13:14:42 -0800

    First commit

---
+++ back-end/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+
+app.listen(3001, () => {
+    console.log(
+      `Listening on port 3001 👍`
+    );
+  });
